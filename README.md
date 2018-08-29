13f6e04ea8afe961ec4f75e74c71d43782398240
Gonzalo Uceda Castro <gonzalouceda@2018-EMEA-0227.local>
2018-08-29 13:06:47 +0200

    created readme

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # mockserver # mockserver
+
+//GENERAL INFO ------> https://github.com/typicode/json-server
+//RUN -------> json-server --watch app.js --port 3004
+//API -------> http://localhost:3004/db ; http://localhost:3004/columnDefs
--- app.js
+++ app.js
@@ -1,6 +1,4 @@
-//GENERAL INFO ------> https://github.com/typicode/json-server
-//RUN -------> json-server --watch app.js --port 3004
-//API -------> http://localhost:3004/db ; http://localhost:3004/columnDefs
+
 
 module.exports = () => {
   const data  = {
